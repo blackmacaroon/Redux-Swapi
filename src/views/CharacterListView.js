9cf293838b83bf2cd7465c147c96c922c2220a3f
Kayla <6lackmacaroon@gmail.com>
2019-05-15 14:44:57 -0600

    mapped mapstatetoprops

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import { getCharacters } from '../actions/index';
 // import actions
 
 class CharacterListView extends React.Component {
@@ -11,11 +12,13 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      <h4>brb....</h4>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -28,8 +31,8 @@ class CharacterListView extends React.Component {
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
